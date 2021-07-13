512468162c22a48896c2dfc09f36f491663ac046
RedMadda <maddalenarossaro91@gmail.com>
2021-07-13 10:10:55 +0200

    push

---
+++ src/components/Hero.vue
@@ -0,0 +1,41 @@
+<template>
+ <div>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Chi siamo</a></li>
+        <li><a href="#">Contatti</a></li>
+    </ul>
+ </div>
+    
+</template>
+
+<script>
+export default {
+    name: "Hero"
+}
+</script>
+
+<style lang="scss" scoped>
+li{
+    display: inline;
+}
+ul{
+    list-style-type: none;
+}
+
+a{
+    padding-top: 40px;
+    margin-left:50px;
+}
+// // template{
+// //     background-image: url(".\src\assets\background.webp");
+// // }
+
+div{
+    background-color:darkgoldenrod;
+    width: 100%;
+    height: 70vh;
+}
+
+ 
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+export default {
+    name: "Title"
+}
+</script>
+
+<style lang="scss" scoped>
+h1{
+    font-size: 40px;
+    color:brown;
+    background-color: burlywood;
+}
+</style>
